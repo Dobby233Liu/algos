ac214ca1164582ed2ecda7f51f2ea168a3b2c7c7
Dobby233Liu <15816141883@163.com>
2021-07-03 19:33:17 +0800

    proper UNUSED macro

--- include/tests.h
+++ include/tests.h
@@ -6,7 +6,10 @@
 #define END_TEST printf("\nTest finished - Good job!\nEND OF TEST\n"); \
         return 0; \
       }
+
 #define UNIT(n) printf("\nUnit %d\n", n);
 #define UNIT_END \
       printf("\nOK\n");
-#define VOID(m) m;
+
+#define VOID(m) (void)m;
+#define UNUSED VOID
