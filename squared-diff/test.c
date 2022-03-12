480a216dab72b43bbb3f552845e46914ab0afe0d
Liu Wenyuan <15816141883@163.com>
2022-03-12 14:23:51 +0800

    [skip ci]

--- squared-diff/test.c
+++ squared-diff/test.c
@@ -2,20 +2,20 @@
 #include "../include/tests_etime.h"
 #include "sdif.h"
 
-TEST
-  UNIT(1)
+TEST {
+  UNIT(1) {
     assert (SQD(100, 30) == 9100);
     assert (SQD_SLOW(100, 30) == 9100);
     assert (SQD(100, 30) == SQD_SLOW(100, 30));
-  UNIT_END
-  UNIT(2)
+  }
+  UNIT(2) {
     UNIT(201)
-    TEST_ETIME
+    TEST_ETIME {
       VOID(SQD(128, 64))
-    TEST_ETIME_END
+    }
     UNIT(202)
-    TEST_ETIME
+    TEST_ETIME {
       VOID(SQD_SLOW(128, 64))
-    TEST_ETIME_END
-  UNIT_END
-END_TEST
+    }
+  }
+}
