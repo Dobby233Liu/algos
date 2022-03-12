42e0789512897884694adb0e6fb011f6d212c435
Liu Wenyuan <15816141883@163.com>
2022-03-12 14:26:37 +0800

    [skip ci]

--- palindrome/test.c
+++ palindrome/test.c
@@ -6,9 +6,9 @@ int b[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 int c[1] = {1};
 int d[2] = {1, 1};
 
-TEST
+TEST {
   assert (is_palindrome(a, 8));
   assert (!is_palindrome(b, 8));
   assert (is_palindrome(c, 1));
   assert (is_palindrome(d, 2));
-END_TEST
+}
